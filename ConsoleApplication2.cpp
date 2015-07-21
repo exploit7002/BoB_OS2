3fca6b1da051c3242aa6cf2338f01f253e0bfb96
exploit7002 <exploit7002@gmail.com>
2015-07-21 14:06:04 +0900

    bob2
    bob2

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,102 @@
+﻿#include <stdio.h>
+#include "stdafx.h"
+#include <malloc.h>
+#include <string.h>
+#include <Windows.h>
+#include <strsafe.h>
+#include <Shlwapi.h>
+#include <stdint.h>
+#include <crtdbg.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include "FileIoHelperClass.h"
+#include <mmio.h>
+
+class StopWatch
+{
+private:
+	LARGE_INTEGER   mFreq, mStart, mEnd;
+	float                   mTimeforDuration;
+public:
+	StopWatch() : mTimeforDuration(0)
+	{
+		mFreq.LowPart = mFreq.HighPart = 0;
+		mStart = mFreq;
+		mEnd = mFreq;
+		QueryPerformanceFrequency(&mFreq);
+	}
+	~StopWatch()
+	{
+	}
+
+public:
+	void Start(){ QueryPerformanceCounter(&mStart); }
+	void Stop()
+	{
+		QueryPerformanceCounter(&mEnd);
+		mTimeforDuration = (mEnd.QuadPart - mStart.QuadPart) / (float)mFreq.QuadPart;
+	}
+	float GetDurationSecond() { return mTimeforDuration; }
+	float GetDurationMilliSecond() { return mTimeforDuration * 1000.f; }
+
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+#define KB 1024
+	uint64_t uFileSize = 4 * KB;
+	_ASSERTE(create_very_big_file(L"bigf.txt", uFileSize));
+
+	StopWatch sw;
+	sw.Start();
+	_ASSERTE(file_copy_using_read_write(L"bigf.txt", L"bigf2.txt"));
+	sw.Stop();
+	print("info] time elapsed = %f", sw.GetDurationSecond());
+
+#if 0 // 湲곗〈�� Memory_Mapped_IO
+	StopWatch sw2;
+	sw2.Start();
+	_ASSERTE(file_copy_using_memory_map(L"bigf.txt", L"bigf3.txt"));
+	sw2.Stop();
+	print("info] time elapsed = %f", sw2.GetDurationSecond());
+#endif
+
+	StopWatch sw2;
+	sw2.Start();
+	LARGE_INTEGER *currOffset = new LARGE_INTEGER;
+	currOffset->QuadPart = (LONGLONG)0;
+
+	LARGE_INTEGER *destOffset = new LARGE_INTEGER;
+	destOffset->QuadPart = (LONGLONG)(1024 * 1024) * (LONGLONG)uFileSize;
+
+	FileIoHelper *aFileIoHelper = new FileIoHelper;
+	aFileIoHelper->FIOpenForRead(L"bigf.txt"); // read
+	aFileIoHelper->FIOCreateFile(L"bigf3.txt", *destOffset); // createfile
+
+	LONGLONG nResult;
+	uint64_t uFileSize_Copy = uFileSize;
+
+	unsigned char *buffer = new unsigned char[uFileSize];
+
+	while (1){
+		nResult = (destOffset->QuadPart - currOffset->QuadPart);
+
+		if (nResult <= 0) break; // infinite loop escape
+
+		(nResult > uFileSize_Copy) ? uFileSize_Copy = uFileSize : uFileSize_Copy = nResult;
+
+		aFileIoHelper->FIOReadFromFile(*currOffset, uFileSize_Copy, buffer);
+		aFileIoHelper->FIOWriteToFile(*currOffset, uFileSize_Copy, buffer);
+
+		currOffset->QuadPart += (LONGLONG)uFileSize_Copy;
+	}
+	sw2.Stop();
+	print("info] time elapsed = %f", sw2.GetDurationSecond());
+
+	delete aFileIoHelper;
+	delete buffer;
+	delete currOffset;
+	delete destOffset;
+	return 0;
+}
---
+++ FileIoHelperClass.cpp
@@ -0,0 +1,377 @@
+﻿/**----------------------------------------------------------------------------
+* FileIoHelperClass.cpp
+*-----------------------------------------------------------------------------
+*
+*-----------------------------------------------------------------------------
+* All rights reserved by somma (fixbrain@gmail.com, unsorted@msn.com)
+*-----------------------------------------------------------------------------
+* 13:10:2011   17:04 created
+**---------------------------------------------------------------------------*/
+#include "stdafx.h"
+
+#include "FileIoHelperClass.h"
+
+/**----------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+FileIoHelper::FileIoHelper()
+	: mReadOnly(TRUE),
+	mFileHandle(INVALID_HANDLE_VALUE),
+	mFileMap(NULL),
+	mFileView(NULL)
+{
+	mFileSize.QuadPart = 0;
+}
+
+/**--------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+FileIoHelper::~FileIoHelper()
+{
+	this->FIOClose();
+}
+
+/**----------------------------------------------------------------------------
+\brief  
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+DTSTATUS
+FileIoHelper::FIOpenForRead(
+IN std::wstring FilePath
+)
+{
+	if (TRUE == Initialized()) { FIOClose(); }
+
+	mReadOnly = TRUE;
+	if (TRUE != is_file_existsW(FilePath.c_str()))
+	{
+	}
+
+#pragma warning(disable: 4127)
+	DTSTATUS status = DTS_WINAPI_FAILED;
+	do
+	{
+		mFileHandle = CreateFileW(
+			FilePath.c_str(),
+			GENERIC_READ,
+			NULL,
+			NULL,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
+			NULL
+			);
+		if (INVALID_HANDLE_VALUE == mFileHandle)
+		{
+		}
+
+		// check file size 
+		// 
+		if (TRUE != GetFileSizeEx(mFileHandle, &mFileSize))
+		{
+			break;
+		}
+
+		mFileMap = CreateFileMapping(
+			mFileHandle,
+			NULL,
+			PAGE_READONLY,
+			0,
+			0,
+			NULL
+			);
+		if (NULL == mFileMap)
+		{
+		}
+
+		status = DTS_SUCCESS;
+	} while (FALSE);
+#pragma warning(default: 4127)
+
+	if (TRUE != DT_SUCCEEDED(status))
+	{
+		if (INVALID_HANDLE_VALUE != mFileHandle)
+		{
+			CloseHandle(mFileHandle);
+			mFileHandle = INVALID_HANDLE_VALUE;
+		}
+		if (NULL != mFileMap) CloseHandle(mFileMap);
+	}
+
+	return status;
+}
+
+/**----------------------------------------------------------------------------
+\brief		
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+DTSTATUS
+FileIoHelper::FIOCreateFile(
+IN std::wstring FilePath,
+IN LARGE_INTEGER FileSize
+)
+{
+	if (TRUE == Initialized()) { FIOClose(); }
+	if (FileSize.QuadPart == 0) return DTS_INVALID_PARAMETER;
+
+	mReadOnly = FALSE;
+
+#pragma warning(disable: 4127)
+	DTSTATUS status = DTS_WINAPI_FAILED;
+	do
+	{
+		mFileSize = FileSize;
+
+		mFileHandle = CreateFileW(
+			FilePath.c_str(),
+			GENERIC_READ | GENERIC_WRITE,
+			FILE_SHARE_READ,		// write ���� �ٸ� ���μ������� �бⰡ ����
+			NULL,
+			CREATE_ALWAYS,
+			FILE_ATTRIBUTE_NORMAL,
+			NULL
+			);
+		if (INVALID_HANDLE_VALUE == mFileHandle)
+		{
+		}
+
+		// increase file size
+		// 
+		if (TRUE != SetFilePointerEx(mFileHandle, mFileSize, NULL, FILE_BEGIN))
+		{
+		}
+
+		if (TRUE != SetEndOfFile(mFileHandle))
+		{
+		}
+
+		mFileMap = CreateFileMapping(
+			mFileHandle,
+			NULL,
+			PAGE_READWRITE,
+			0,
+			0,
+			NULL
+			);
+		if (NULL == mFileMap)
+		{
+		}
+
+		status = DTS_SUCCESS;
+	} while (FALSE);
+#pragma warning(default: 4127)
+
+	if (TRUE != DT_SUCCEEDED(status))
+	{
+		if (INVALID_HANDLE_VALUE != mFileHandle)
+		{
+			CloseHandle(mFileHandle);
+			mFileHandle = INVALID_HANDLE_VALUE;
+		}
+		if (NULL != mFileMap) CloseHandle(mFileMap);
+	}
+
+	return status;
+}
+
+/**----------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+void
+FileIoHelper::FIOClose(
+)
+{
+	if (TRUE != Initialized()) return;
+
+	FIOUnreference();
+	CloseHandle(mFileMap); mFileMap = NULL;
+	CloseHandle(mFileHandle); mFileHandle = INVALID_HANDLE_VALUE;
+}
+
+/**----------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+DTSTATUS
+FileIoHelper::FIOReference(
+IN BOOL ReadOnly,
+IN LARGE_INTEGER Offset,
+IN DWORD Size,
+IN OUT PUCHAR& ReferencedPointer
+)
+{
+	if (TRUE != Initialized()) return DTS_INVALID_OBJECT_STATUS;
+	if (TRUE == IsReadOnly())
+	{
+		if (TRUE != ReadOnly)
+		{
+		}
+	}
+
+	_ASSERTE(NULL == mFileView);
+	FIOUnreference();
+
+	if (Offset.QuadPart + Size > mFileSize.QuadPart)
+	{
+	}
+
+	static DWORD AllocationGranularity = 0;
+	if (0 == AllocationGranularity)
+	{
+		SYSTEM_INFO si = { 0 };
+		GetSystemInfo(&si);
+		AllocationGranularity = si.dwAllocationGranularity;
+	}
+
+	DWORD AdjustMask = AllocationGranularity - 1;
+	LARGE_INTEGER AdjustOffset = { 0 };
+	AdjustOffset.HighPart = Offset.HighPart;
+
+
+	AdjustOffset.LowPart = (Offset.LowPart & ~AdjustMask);
+
+
+	DWORD BytesToMap = (Offset.LowPart & AdjustMask) + Size;
+
+	mFileView = (PUCHAR)MapViewOfFile(
+		mFileMap,
+		(TRUE == ReadOnly) ? FILE_MAP_READ : FILE_MAP_READ | FILE_MAP_WRITE,
+		AdjustOffset.HighPart,
+		AdjustOffset.LowPart,
+		BytesToMap
+		);
+	if (NULL == mFileView)
+	{
+	}
+
+	ReferencedPointer = &mFileView[Offset.LowPart & AdjustMask];
+	return DTS_SUCCESS;
+}
+
+/**----------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+void
+FileIoHelper::FIOUnreference(
+)
+{
+	if (NULL != mFileView)
+	{
+		UnmapViewOfFile(mFileView);
+		mFileView = NULL;
+	}
+}
+
+/**----------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+DTSTATUS
+FileIoHelper::FIOReadFromFile(
+IN LARGE_INTEGER Offset,
+IN DWORD Size,
+IN OUT PUCHAR Buffer
+)
+{
+	_ASSERTE(NULL != Buffer);
+	if (NULL == Buffer) return DTS_INVALID_PARAMETER;
+
+	UCHAR* p = NULL;
+	DTSTATUS status = FIOReference(TRUE, Offset, Size, p);
+	if (TRUE != DT_SUCCEEDED(status))
+	{
+	}
+
+	__try
+	{
+		RtlCopyMemory(Buffer, p, Size);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
+
+	FIOUnreference();
+	return status;
+}
+
+/**----------------------------------------------------------------------------
+\brief
+
+\param
+\return
+\code
+
+\endcode
+-----------------------------------------------------------------------------*/
+DTSTATUS
+FileIoHelper::FIOWriteToFile(
+IN LARGE_INTEGER Offset,
+IN DWORD Size,
+IN PUCHAR Buffer
+)
+{
+	_ASSERTE(NULL != Buffer);
+	_ASSERTE(0 != Size);
+	_ASSERTE(NULL != Buffer);
+	if (NULL == Buffer || 0 == Size || NULL == Buffer) return DTS_INVALID_PARAMETER;
+
+	UCHAR* p = NULL;
+	DTSTATUS status = FIOReference(FALSE, Offset, Size, p);
+	if (TRUE != DT_SUCCEEDED(status))
+	{
+	}
+
+	__try
+	{
+		RtlCopyMemory(p, Buffer, Size);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
+
+	FIOUnreference();
+	return status;
+}
